fd942dd01c01b1e907dd8aa65375d8d0c570fe6c
Sebastian Pavel <59478289+zerK4@users.noreply.github.com>
2022-06-24 18:12:11 +0300

    Update now.js

--- pages/api/now.js
+++ pages/api/now.js
@@ -1,5 +1,5 @@
 import data from './data.json'
 export default function New(req, res){
-    res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+    res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate", "Access-Control-Allow-Origin", "*" );
     res.json(data)
 }
