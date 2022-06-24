c74d120afddaa8c9cd12091204eef46609087579
Sebastian Pavel <59478289+zerK4@users.noreply.github.com>
2022-06-24 17:53:39 +0300

    Delete now.js

--- pages/api/date/now.js
+++
@@ -1,8 +0,0 @@
-export default function New(req, res){
-    const date = new Date()
-    const format = date.toString();
-
-    res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
-
-    res.json({now: format})
-}
