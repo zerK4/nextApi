fffd4d56648896578d4298a8180de6ac7dc52870
Sebastian Pavel <59478289+zerK4@users.noreply.github.com>
2022-06-24 17:49:05 +0300

    Update now.js

--- pages/api/date/now.js
+++ pages/api/date/now.js
@@ -2,5 +2,7 @@ export default function New(req, res){
     const date = new Date()
     const format = date.toString();
 
+    res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
     res.json({now: format})
 }
