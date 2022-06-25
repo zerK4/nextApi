03cab5aa8af71ded9fd0d56469a7e14dd96219bf
Sebastian Pavel <59478289+zerK4@users.noreply.github.com>
2022-06-25 09:57:34 +0300

    Update now.js

--- pages/api/now.js
+++ pages/api/now.js
@@ -1,7 +1,7 @@
 import data from './data.json'
 export default function New(req, res){
     res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
-    res.setHeader("access-control-allow-origin", "*")
-    res.setHeader("access-control-allow-headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
     res.json(data)
-}
+}   
